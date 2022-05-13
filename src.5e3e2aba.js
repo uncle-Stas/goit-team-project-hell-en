0e4898796e84bb9eeedfdff6f25e75ff8f5ddc1b
uncle-Stas <101670941+uncle-Stas@users.noreply.github.com>
2022-05-13 10:35:46 +0000

    Deploying to gh-pages from @ uncle-Stas/goit-team-project-hell-en@ca0fe135b9adfa3c99b7dad69490517da40d2b55 🚀

--- src.5e3e2aba.js
+++ src.5e3e2aba.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-team-project-hell-en/src.6d13fcd7.js.map
+//# sourceMappingURL=/goit-team-project-hell-en/src.5e3e2aba.js.map
