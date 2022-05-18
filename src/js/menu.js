dfe6a18129156cc2b71960f2025a1996057617f9
uncle-Stas <stanislav.zavadskyi11@gmail.com>
2022-05-18 15:55:05 +0300

    отмена прокрутки сайта при активной мобильной нави

--- src/js/menu.js
+++ src/js/menu.js
@@ -3,6 +3,7 @@
     openMenuBtn: document.querySelector('[data-menu-open]'),
     closeMenuBtn: document.querySelector('[data-menu-close]'),
     menu: document.querySelector('[data-menu]'),
+    bodyHide: document.querySelector("[data-menu-body]"),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleModal);
@@ -10,5 +11,6 @@
 
   function toggleModal() {
     refs.menu.classList.toggle('is-open');
+    refs.bodyHide.classList.toggle("body-hide");
   }
 })();
