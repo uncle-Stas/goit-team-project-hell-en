6c5cc97e3a58813b599111f417e3d1398c6574c1
uncle-Stas <stanislav.zavadskyi11@gmail.com>
2022-05-18 18:29:07 +0300

    финальное редактирование

--- src/js/menu.js
+++ src/js/menu.js
@@ -4,10 +4,12 @@
     closeMenuBtn: document.querySelector('[data-menu-close]'),
     menu: document.querySelector('[data-menu]'),
     bodyHide: document.querySelector("[data-menu-body]"),
+    mobileMenu: document.querySelector('.mobile-menu-site-nav'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleModal);
   refs.closeMenuBtn.addEventListener('click', toggleModal);
+  refs.mobileMenu.addEventListener('click', toggleModal)
 
   function toggleModal() {
     refs.menu.classList.toggle('is-open');
